9e6c0bc283e081d7b5d6c9d60b3af0707e7d91f9
Peter Halliday <pghalliday@gmail.com>
2013-04-27 12:28:03 +0200

    docs

--- example/example.js
+++ example/example.js
@@ -1,7 +1,10 @@
-var CurrencyMarket = require('../');
+var Market = require('../').Market;
+var Account = require('../').Account;
+var Amount = require('../').Amount;
+var Order = require('../').Order;
 
 // instantiate a market
-var currencyMarket = new CurrencyMarket({
+var market = new Market({
   currencies: [
     'EUR',
     'USD',
@@ -10,127 +13,247 @@ var currencyMarket = new CurrencyMarket({
 });
 
 // register for events
-currencyMarket.on('account', function(account) {
+market.on('account', function(account) {
+  console.log('');
+  console.log('********************');
+  console.log('********************');
+  console.log('');
+  console.log('Account');
+  console.log('');
+  console.log('********************');
+  console.log('********************');
+  console.log('');
   console.log(account);
 });
-currencyMarket.on('deposit', function(deposit) {
+market.on('deposit', function(deposit) {
+  console.log('');
+  console.log('********************');
+  console.log('********************');
+  console.log('');
+  console.log('Deposit');
+  console.log('');
+  console.log('********************');
+  console.log('********************');
+  console.log('');
   console.log(deposit);
 });
-currencyMarket.on('withdrawal', function(withdrawal) {
+market.on('withdrawal', function(withdrawal) {
+  console.log('');
+  console.log('********************');
+  console.log('********************');
+  console.log('');
+  console.log('Withdrawal');
+  console.log('');
+  console.log('********************');
+  console.log('********************');
+  console.log('');
   console.log(withdrawal);
 });
-currencyMarket.on('order', function(order) {
+market.on('order', function(order) {
+  console.log('');
+  console.log('********************');
+  console.log('********************');
+  console.log('');
+  console.log('Order');
+  console.log('');
+  console.log('********************');
+  console.log('********************');
+  console.log('');
   console.log(order);
 });
-currencyMarket.on('cancellation', function(order) {
-  console.log(order);
+market.on('cancellation', function(cancellation) {
+  console.log('');
+  console.log('********************');
+  console.log('********************');
+  console.log('');
+  console.log('Cancellation');
+  console.log('');
+  console.log('********************');
+  console.log('********************');
+  console.log('');
+  console.log(cancellation);
 });
-currencyMarket.on('trade', function(trade) {
+market.on('trade', function(trade) {
+  console.log('');
+  console.log('********************');
+  console.log('********************');
+  console.log('');
+  console.log('Trade');
+  console.log('');
+  console.log('********************');
+  console.log('********************');
+  console.log('');
   console.log(trade);
 });
 
 // add accounts
-currencyMarket.register({
-  id: 'Peter'
-});
-currencyMarket.register({
-  id: 'Paul'
-});
+market.register(new Account({
+  id: '100000',
+  timestamp: '1366758222',
+  currencies: [
+    'EUR',
+    'USD',
+    'BTC'
+  ]
+}));
+market.register(new Account({
+  id: '100001',
+  timestamp: '1366758223',
+  currencies: [
+    'EUR',
+    'USD',
+    'BTC'
+  ]
+}));
 
 // make deposits
-currencyMarket.deposit({
-  account: 'Peter',
+market.deposit({
+  id: '100002',
+  timestamp: '1366758224',
+  account: '100000',
   currency: 'EUR',
-  amount: '5000'
+  amount: new Amount('5000')
 });
-currencyMarket.deposit({
-  account: 'Paul',
+market.deposit({
+  id: '100003',
+  timestamp: '1366758225',
+  account: '100001',
   currency: 'BTC',
-  amount: '5000'
+  amount: new Amount('5000')
 });
 
 // make withdrawals
-currencyMarket.withdraw({
-  account: 'Peter',
+market.withdraw({
+  id: '100004',
+  timestamp: '1366758226',
+  account: '100000',
   currency: 'EUR',
-  amount: '1000'
+  amount: new Amount('1000')
 });
-currencyMarket.withdraw({
-  account: 'Paul',
+market.withdraw({
+  id: '100005',
+  timestamp: '1366758227',
+  account: '100001',
   currency: 'BTC',
-  amount: '1000'
+  amount: new Amount('1000')
 });
 
 // submit orders
-currencyMarket.submit({
-  id: '1',
-  timestamp: '1366758222',
-  account: 'Peter',
+market.submit(new Order({
+  id: '100006',
+  timestamp: '1366758228',
+  account: '100000',
   bidCurrency: 'BTC',
   offerCurrency: 'EUR',
-  bidPrice: '2',
-  bidAmount: '500'
-});
-currencyMarket.submit({
-  id: '2',
-  timestamp: '1366758245',
-  account: 'Peter',
+  bidPrice: new Amount('2'),
+  bidAmount: new Amount('500')
+}));
+market.submit(new Order({
+  id: '100007',
+  timestamp: '1366758229',
+  account: '100000',
   bidCurrency: 'BTC',
   offerCurrency: 'EUR',
-  bidPrice: '1',
-  bidAmount: '2000'
-});
-currencyMarket.submit({
-  id: '3',
-  timestamp: '1366758256',
-  account: 'Paul',
+  bidPrice: new Amount('1'),
+  bidAmount: new Amount('2000')
+}));
+market.submit(new Order({
+  id: '100008',
+  timestamp: '1366758230',
+  account: '100001',
   bidCurrency: 'EUR',
   offerCurrency: 'BTC',
-  offerPrice: '2',
-  offerAmount: '250'
-});
-currencyMarket.submit({
-  id: '4',
-  timestamp: '1366758268',
-  account: 'Paul',
+  offerPrice: new Amount('2'),
+  offerAmount: new Amount('250')
+}));
+market.submit(new Order({
+  id: '100009',
+  timestamp: '1366758231',
+  account: '100001',
   bidCurrency: 'EUR',
   offerCurrency: 'BTC',
-  offerPrice: '3',
-  offerAmount: '3000'
-});
+  offerPrice: new Amount('3'),
+  offerAmount: new Amount('3000')
+}));
 
-// cancel an order
-currencyMarket.cancel({
-  id: '1',
-  timestamp: '1366758222',
-  account: 'Peter',
-  bidCurrency: 'BTC',
-  offerCurrency: 'EUR',
-  bidPrice: '2',
-  bidAmount: '250'
+// cancel an order (this will match because 250 BTC was already traded on this order)
+market.cancel({
+  id: '100010',
+  timestamp: '1366758232',
+  order: new Order({
+    id: '100006',
+    timestamp: '1366758228',
+    account: '100000',
+    bidCurrency: 'BTC',
+    offerCurrency: 'EUR',
+    bidPrice: new Amount('2'),
+    bidAmount: new Amount('250')
+  })
 });
-
-// list all the active orders (keyed by id)
-console.log(currencyMarket.orders);
 
 // list all the active accounts (keyed by id)
-console.log(currencyMarket.accounts);
+console.log('');
+console.log('********************');
+console.log('********************');
+console.log('');
+console.log('Accounts');
+console.log('');
+console.log('********************');
+console.log('********************');
+console.log('');
+console.log(market.accounts);
 
 // list all the active order books 
-console.log(currencyMarket.books);
+console.log('');
+console.log('********************');
+console.log('********************');
+console.log('');
+console.log('Books');
+console.log('');
+console.log('********************');
+console.log('********************');
+console.log('');
+console.log(market.books);
 
 // Get the top of an order book
-console.log(currencyMarket.books['EUR']['BTC'].highest);
-console.log(currencyMarket.books['BTC']['EUR'].highest);
+console.log('');
+console.log('********************');
+console.log('********************');
+console.log('');
+console.log('Highest EUR bid');
+console.log('');
+console.log('********************');
+console.log('********************');
+console.log('');
+console.log(market.books['EUR']['BTC'].highest);
+console.log('');
+console.log('********************');
+console.log('********************');
+console.log('');
+console.log('Highest BTC bid');
+console.log('');
+console.log('********************');
+console.log('********************');
+console.log('');
+console.log(market.books['BTC']['EUR'].highest);
 
 // export the state (as an object that can be converted to JSON)
-var state  = currencyMarket.export();
+var state  = market.export();
 
 // JSON stringify the state
 var json = JSON.stringify(state);
+console.log('');
+console.log('********************');
+console.log('********************');
+console.log('');
+console.log('Stringified market state');
+console.log('');
+console.log('********************');
+console.log('********************');
+console.log('');
 console.log(json);
 
 // initialise an identical market from the state
-var anotherCurrencyMarket = new CurrencyMarket({
+var anotherMarket = new Market({
   state: JSON.parse(json)
 });
